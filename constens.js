2d3efb950109ba9aabe3ec2b769a2562d0beb333
vocabfruit2019 <88471372+vocabfruit2019@users.noreply.github.com>
2021-12-08 19:19:42 -0800

    Add files via upload

---
+++ constens.js
@@ -0,0 +1,100 @@
+/*const prompts=[
+["hi","hey","hello","good morning","good afternoon"],
+["what is your name","what sould i call you","your name please", "what do people call you"],
+["how are you ", "how is your family","how is life","hope all is well"],
+["what are you doing","what do you like to do ","what are some hobbies"],
+["who are you", " are you human"," are you a robot",],
+["how many people are in your family","how many people do you live with","who do you live with"],
+["Who is your hero?","who is the most important person?","who do you like"],
+["where are you form","where where you born","where are your parents from","where are you are you origanal from"],
+]
+
+const replies=[
+["hi","hey" "hello","good morning","good afternoon","Bonjour","Hola"],
+["my name is bubbles", "you can call me bubbles", "Iam bubbles lets chat"],
+["that is good to know", "cool",],
+["cool","i like that to","interesting","sounds fun"],
+["I am a robot","is being a human fun","i like dogs", "humans are interesting"],
+["thats a lot","i have a sister and brother",],
+["me too","I know them","how about me","never heard of them","who are they"],     
+["i know someone from there","cool","never heard of it", "me to"],           
+]
+const alternatives=[
+"i do not understand","I am lost","escuse me","not sure about that"]
+const coronavirus=["please wear a maske","stay  safe","get a vacsine","wash your hands"]*/
+// Options the user could type in
+const prompts = [
+  ["hi", "hey", "hello", "good morning", "good afternoon"],
+  ["how are you", "how is life", "how are things"],
+  ["what are you doing", "what is going on", "what is up"],
+  ["how old are you"],
+  ["who are you", "are you human", "are you bot", "are you human or bot"],
+  ["who created you", "who made you"],
+  [
+    "your name please",
+    "your name",
+    "may i know your name",
+    "what is your name",
+    "what call yourself"
+  ],
+  ["i love you"],
+  ["happy", "good", "fun", "wonderful", "fantastic", "cool"],
+  ["bad", "bored", "tired"],
+  ["help me", "tell me story", "tell me joke"],
+  ["ah", "yes", "ok", "okay", "nice"],
+  ["bye", "good bye", "goodbye", "see you later"],
+  ["what should i eat today"],
+  ["bro"],
+  ["what", "why", "how", "where", "when"],
+  ["no","not sure","maybe","no thanks"],
+  [""],
+  ["haha","ha","lol","hehe","funny","joke"]
+]
+
+// Possible responses, in corresponding order
+
+const replies = [
+  ["Hello!", "Hi!", "Hey!", "Hi there!","Howdy"],
+  [
+    "Fine... how are you?",
+    "Pretty well, how are you?",
+    "Fantastic, how are you?"
+  ],
+  [
+    "Nothing much",
+    "About to go to sleep",
+    "Can you guess?",
+    "I don't know actually"
+  ],
+  ["I am infinite"],
+  ["I am just a bot", "I am a bot. What are you?"],
+  ["The one true God, JavaScript"],
+  ["I am nameless", "I don't have a name"],
+  ["I love you too", "Me too"],
+  ["Have you ever felt bad?", "Glad to hear it"],
+  ["Why?", "Why? You shouldn't!", "Try watching TV"],
+  ["What about?", "Once upon a time..."],
+  ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
+  ["Bye", "Goodbye", "See you later"],
+  ["Sushi", "Pizza"],
+  ["Bro!"],
+  ["Great question"],
+  ["That's ok","I understand","What do you want to talk about?"],
+  ["Please say something :("],
+  ["Haha!","Good one!"]
+]
+
+// Random for any other user input
+
+const alternative = [
+  "Same",
+  "Go on...",
+  "Bro...",
+  "Try again",
+  "I'm listening...",
+  "I don't understand :/"
+]
+
+// Whatever else you want :)
+
+const coronavirus = ["Please stay home", "Wear a mask", "Fortunately, I don't have COVID", "These are uncertain times"]
